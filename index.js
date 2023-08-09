93c8125f63861d4630914f4952166fe500a951b0
velan-3 <106436639+velan-3@users.noreply.github.com>
2023-08-09 23:01:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const http = require('http');
-const PORT = 3000;
+const express = require('express');
+const axios = require('axios');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+const app = express();
+const port = 3000;
+
+const apiKey = 'rzp_test_RGffn7bgBbJOjw'; // Replace with your actual Razorpay API Key
+const apiSecret = 'bUk6F2XK8n0iNoeU7pWW1muO'; // Replace with your actual Razorpay API Secret
+
+const baseUrl = 'https://api.razorpay.com/v1';
+
+const authHeader = `Basic ${Buffer.from(`${apiKey}:${apiSecret}`).toString('base64')}`;
+
+app.get('/payment/:paymentId', async (req, res) => {
+  const paymentId = req.params.paymentId;
+
+  try {
+    const response = await axios.get(`${baseUrl}/payments/${paymentId}`, {
+      headers: {
+        Authorization: authHeader,
+      },
+    });
+
+    const paymentDetails = response.data;
+    res.json(paymentDetails);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching payment details' });
+  }
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
